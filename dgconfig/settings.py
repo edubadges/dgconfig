677a4fcabb9a73c38d5cd99579ad223852d06974
Fako Berkers <email@fakoberkers.nl>
2018-12-14 16:23:06 +0100

    Adds a user agent configuration

--- dgconfig/settings.py
+++ dgconfig/settings.py
@@ -9,10 +9,12 @@ import os
 DATAGROWTH_DEFAULT_CONFIGURATION = {
     "global_purge_immediately": False,  # by default keep resources around
     "global_sample_size": 0,
+    "global_user_agent": "DataGrowth (0.0.1)"
 }
 
 
 DATAGROWTH_MOCK_CONFIGURATION = {
     "global_purge_immediately": False,
     "global_sample_size": 0,
+    "global_user_agent": "DataGrowth (0.0.1)"
 }
